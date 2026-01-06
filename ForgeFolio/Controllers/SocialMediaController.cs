434bf7be3ee6da31f4da214f959dad10920da9b0
Anıl Akbay <anilakbay20@gmail.com>
2026-01-07 01:05:32 +0300

    SocialMedia ve Testimonial CRUD tamamlandı + Security cleanup
    - SocialMediaController: CRUD actions eklendi
    - TestimonialController: CRUD actions eklendi
    - SocialMedia views: Index + Create + Update
    - Testimonial Index: Card layout + star ratings
    - 19 publish dosyası silindi (güvenlik)

--- ForgeFolio/Controllers/SocialMediaController.cs
+++ ForgeFolio/Controllers/SocialMediaController.cs
@@ -9,9 +9,46 @@ namespace ForgeFolio.Controllers;
 // [Authorize(Roles = "Admin")] // Temporarily disabled
 public class SocialMediaController : Controller
 {
-    // Simple Index action - no service needed yet
     public IActionResult Index()
     {
+        // TODO: Fetch real data
         return View();
+    }
+
+    [HttpGet]
+    public IActionResult CreateSocialMedia()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult CreateSocialMedia(string Title, string Url, string Icon)
+    {
+        // TODO: Service integration
+        TempData["SuccessMessage"] = "Sosyal medya hesabı başarıyla eklendi!";
+        return RedirectToAction(nameof(Index));
+    }
+
+    [HttpGet]
+    public IActionResult UpdateSocialMedia(int id)
+    {
+        // TODO: Fetch by id
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult UpdateSocialMedia(int id, string Title, string Url, string Icon)
+    {
+        // TODO: Service integration
+        TempData["SuccessMessage"] = "Sosyal medya hesabı başarıyla güncellendi!";
+        return RedirectToAction(nameof(Index));
+    }
+
+    [HttpGet]
+    public IActionResult DeleteSocialMedia(int id)
+    {
+        // TODO: Service integration
+        TempData["SuccessMessage"] = "Sosyal medya hesabı silindi!";
+        return RedirectToAction(nameof(Index));
     }
 }
--- ForgeFolio/Controllers/TestimonialController.cs
+++ ForgeFolio/Controllers/TestimonialController.cs
@@ -9,9 +9,46 @@ namespace ForgeFolio.Controllers;
 // [Authorize(Roles = "Admin")] // Temporarily disabled
 public class TestimonialController : Controller
 {
-    // Simple Index action - no service needed yet
     public IActionResult Index()
     {
+        // TODO: Fetch real data
         return View();
+    }
+
+    [HttpGet]
+    public IActionResult CreateTestimonial()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult CreateTestimonial(string ClientName, string Company, string Comment, int Rating)
+    {
+        // TODO: Service integration
+        TempData["SuccessMessage"] = "Testimonial başarıyla eklendi!";
+        return RedirectToAction(nameof(Index));
+    }
+
+    [HttpGet]
+    public IActionResult UpdateTestimonial(int id)
+    {
+        // TODO: Fetch by id
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult UpdateTestimonial(int id, string ClientName, string Company, string Comment, int Rating)
+    {
+        // TODO: Service integration
+        TempData["SuccessMessage"] = "Testimonial başarıyla güncellendi!";
+        return RedirectToAction(nameof(Index));
+    }
+
+    [HttpGet]
+    public IActionResult DeleteTestimonial(int id)
+    {
+        // TODO: Service integration
+        TempData["SuccessMessage"] = "Testimonial silindi!";
+        return RedirectToAction(nameof(Index));
     }
 }
